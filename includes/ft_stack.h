3c200869f3d01947b3168bdab3fb352beba362e8
AntoineVerin <24255551+VerinAntoine@users.noreply.github.com>
2023-11-24 21:02:13 +0100

    norminetted

--- includes/ft_stack.h
+++ includes/ft_stack.h
@@ -22,19 +22,19 @@ typedef struct s_stack_item {
 
 typedef struct s_stack {
 	t_stack_item	*items;
-	char		name;
+	char			name;
 }	t_stack;
 
-void		stack_swap(t_stack *stack);
-void		stack_push(t_stack *from, t_stack *to);
-void		stack_rotate(t_stack *stack);
-void		stack_rvs_rotate(t_stack *stack);
+void			stack_swap(t_stack *stack);
+void			stack_push(t_stack *from, t_stack *to);
+void			stack_rotate(t_stack *stack);
+void			stack_rvs_rotate(t_stack *stack);
 
-int			stack_is_ordered(t_stack *stack);
-int			stack_add(t_stack *stack, int value);
-void		stack_remove(t_stack *stack, t_stack_item *item);
-void		stack_free(t_stack *stack);
-void		stack_print(t_stack *stack);
+int				stack_is_ordered(t_stack *stack);
+int				stack_add(t_stack *stack, int value);
+void			stack_remove(t_stack *stack, t_stack_item *item);
+void			stack_free(t_stack *stack);
+void			stack_print(t_stack *stack);
 t_stack_item	*stack_last(t_stack *stack);
 
 #endif
